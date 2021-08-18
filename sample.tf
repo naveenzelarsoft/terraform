9ad8e45c025809deaef28aa0dec83eb39bb2b32e
naveen <naveen@gmail.com>
2021-08-18 19:12:42 +0530

    terraform

--- sample.tf
+++ sample.tf
@@ -1,26 +1,33 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_instance" "sample" {
-  ami          = "ami-0eb5f3f64b10d3e0e"
-  instance_type = "t2.micro"
-  tags = {
-    Name = "HelloWorld"
-  }
-}
+
 resource "aws_security_group" "terraform" {
-  name        = "allow_terraform"
+  name = "allow_terraform"
   description = "Allow TF inbound traffic"
   ingress {
     from_port = 22
     protocol = "tcp"
     to_port = 22
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
   }
   egress {
     from_port = 0
     protocol = "-1"
     to_port = 0
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [
+      "0.0.0.0/0"]
+   }
+  tags = {
+    Name = "terraform"
+  }
+}
+resource "aws_instance" "sample" {
+  ami          = "ami-0eb5f3f64b10d3e0e"
+  instance_type = "t2.micro"
+  security_groups = ["${aws_security_group.terraform.name}"]
+  tags = {
+    Name = "HelloWorld"
   }
 }
