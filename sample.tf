2109a9aa5709adf5ea35ca57e9ad591b403af76c
naveen <naveen@gmail.com>
2021-08-18 16:52:39 +0530

    terraform

--- sample.tf
+++ sample.tf
@@ -8,3 +8,25 @@ resource "aws_instance" "sample" {
     Name = "HelloWorld"
   }
 }
+resource "aws_security_group" "allow_terraform" {
+  name        = "allow_terraform"
+  description = "Allow TF inbound traffic"
+
+  ingress = [
+    {
+      description      = "TLS from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+}
