1bc84bd8c44e35e8445a2924ac5f74dbeffe8103
naveen <naveen@gmail.com>
2021-08-18 17:32:12 +0530

    terraform

--- sample.tf
+++ sample.tf
@@ -11,7 +11,7 @@ resource "aws_instance" "sample" {
 resource "aws_security_group" "default" {
   name        = "allow_terraform"
   description = "Allow TF inbound traffic"
-  vpc_id      = vpc-9947e0e4
+  vpc_id      = "vpc-9947e0e4"
   ingress = [
     {
       from_port        = 22
