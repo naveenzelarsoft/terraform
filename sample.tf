a471576c15afa3cae5b2a1a106fc915fdb42ba54
naveen <naveen@gmail.com>
2021-08-18 16:27:02 +0530

    terraform

--- sample.tf
+++ sample.tf
@@ -1,7 +1,6 @@
 resource "aws_instance" "web" {
-  ami          = ami-0eb5f3f64b10d3e0e
+  ami          = "ami-0eb5f3f64b10d3e0e"
   instance_type = "t2.micro"
-
   tags = {
     Name = "HelloWorld"
   }
