028b0f63f3fea6c94aa00ad7fd7dce0b299540a4
naveen <naveen@gmail.com>
2021-10-04 19:02:24 +0530

    sample

--- sample/main.tf
+++ sample/main.tf
@@ -1,5 +1,12 @@
 terraform {
-  required_providers {
+  backend "remote" {
+    organization = "naveen5035"
+
+    workspaces {
+      name = "sample"
+    }
+  }
+required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 3.27"
