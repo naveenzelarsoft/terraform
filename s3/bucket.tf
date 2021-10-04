c8aab74b18dd2ede6df35ae11f499d6caa5219b4
naveen <naveen@gmail.com>
2021-10-04 18:20:28 +0530

    instance

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -2,11 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 resource "aws_s3_bucket" "b" {
-  bucket = "my-tf-test-bucket"
+  bucket = "naveen"
   acl    = "private"
 
   tags = {
-    Name        = "My bucket"
+    Name        = "naveen"
     Environment = "Dev"
   }
 }
