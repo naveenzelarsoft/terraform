11b9e761598e4c78e5e7d0f7c2303460570936bf
naveen <naveen@gmail.com>
2021-10-05 12:03:06 +0530

    sample

--- vpc/main.tf
+++ vpc/main.tf
@@ -19,42 +19,83 @@ required_providers {
 provider "aws" {
   region     = "us-east-1"
 }
-resource "aws_vpc" "vpc" {
-  cidr_block = "10.0.0.0/16"
-}
 
-resource "aws_internet_gateway" "gateway" {
-  vpc_id = "${aws_vpc.vpc.id}"
-}
-resource "aws_route" "route" {
-  route_table_id         = "${aws_vpc.vpc.main_route_table_id}"
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = "${aws_internet_gateway.gateway.id}"
-}
-data "aws_availability_zones" "available" {}
+resource "aws_vpc" "development" { #1
+  cidr_block = "10.0.0.0/16" #2
 
-resource "aws_subnet" "main" {
-  count                   = "${length(data.aws_availability_zones.available.names)}"
-  vpc_id                  = "${aws_vpc.vpc.id}"
-  cidr_block              = "10.0.${count.index}.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "${element(data.aws_availability_zones.available.names, count.index)}"
-}
-
-resource "aws_security_group" "default" {
-  name        = "http-https-allow"
-  description = "Allow incoming HTTP and HTTPS and Connections"
-  vpc_id      = "${aws_vpc.vpc.id}"
-  ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+  tags = {
+    Environment = "development" #3
   }
-  ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+}
+
+resource "aws_subnet" "private" { #1
+  vpc_id     = aws_vpc.development.id #2
+  cidr_block = "10.0.1.0/24" #3
+
+  tags = {
+    Environment = "development"
+    Access = "private"
   }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.development.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Environment = "development"
+    Access = "public"
+  }
+}
+
+resource "aws_internet_gateway" "igw" { #1
+  vpc_id = aws_vpc.development.id
+
+  tags = {
+    Environment = "development"
+  }
+}
+
+
+
+resource "aws_route_table" "public" { #2
+  vpc_id = aws_vpc.development.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+  tags = {
+    Environment = "development"
+  }
+}
+
+resource "aws_route_table_association" "public" { #3
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_eip" "nat" { #1
+  vpc      = true
+}
+
+resource "aws_nat_gateway" "ngw" { #2
+  allocation_id = aws_eip.nat.id #3
+  subnet_id = aws_subnet.public.id #4
+}
+
+resource "aws_route_table" "private" { #5
+  vpc_id = aws_vpc.development.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.ngw.id #6
+  }
+
+  tags = {
+    Environment = "development"
+  }
+}
+
+resource "aws_route_table_association" "private" { #7
+  subnet_id = aws_subnet.private.id
+  route_table_id = aws_route_table.private.id
 }
