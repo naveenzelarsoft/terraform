3621c8c7e71e814b6719cafe82a9aeb8547fea04
naveen <naveen@gmail.com>
2021-10-05 17:01:12 +0530

    sample

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -95,27 +95,27 @@ resource "aws_route_table_association" "public-subnet-3-route-table-association"
   route_table_id      = aws_route_table.public-route-table.id
 }
 
-resource "aws_nat_gateway" "public_subent-1" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.public-subnet-1.id
-  tags      = {
-    Name    = "Nat-1"
-  }
-}
-resource "aws_nat_gateway" "public_subent-2" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.public-subnet-2.id
-  tags      = {
-    Name    = "Nat-2"
-  }
-}
-resource "aws_nat_gateway" "public_subent-3" {
-  connectivity_type = "private"
-  subnet_id         = aws_subnet.public-subnet-3.id
-  tags      = {
-    Name    = "Nat-3"
-  }
-}
+#resource "aws_nat_gateway" "public_subent-1" {
+#  connectivity_type = "private"
+#  subnet_id         = aws_subnet.public-subnet-1.id
+#  tags      = {
+#    Name    = "Nat-1"
+#  }
+#}
+#resource "aws_nat_gateway" "public_subent-2" {
+#  connectivity_type = "private"
+#  subnet_id         = aws_subnet.public-subnet-2.id
+#  tags      = {
+#    Name    = "Nat-2"
+#  }
+#}
+#resource "aws_nat_gateway" "public_subent-3" {
+#  connectivity_type = "private"
+#  subnet_id         = aws_subnet.public-subnet-3.id
+#  tags      = {
+#    Name    = "Nat-3"
+#  }
+#}
 
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                   = aws_vpc.vpc.id
