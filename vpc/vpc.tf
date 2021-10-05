5e1485cde6f224de2a4082fff27e59883382d33f
naveen <naveen@gmail.com>
2021-10-05 18:49:30 +0530

    sample

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -71,7 +71,7 @@ resource "aws_route_table" "public-route-table" {
   vpc_id       = aws_vpc.vpc.id
 
   route {
-    cidr_block = "0.0.0.0/0"
+    cidr_block = "10.0.0.0/0"
     gateway_id = aws_internet_gateway.internet-gateway.id
   }
 
