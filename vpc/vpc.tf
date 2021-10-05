25f60950a52225c09e178b7b4904b6134b56226f
naveen <naveen@gmail.com>
2021-10-05 16:09:14 +0530

    sample

--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -23,7 +23,6 @@ provider "aws" {
 resource "aws_vpc" "vpc" {
   cidr_block              = "${var.vpc-cidr}"
   instance_tenancy        = "default"
-  enable_dns_hostnames    = true
 
   tags      = {
     Name    = "Sample"
@@ -42,7 +41,6 @@ resource "aws_subnet" "public-subnet-1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "${var.public-subnet-1-cidr}"
   availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
 
   tags      = {
     Name    = "Public Subnet 1"
@@ -53,7 +51,6 @@ resource "aws_subnet" "public-subnet-2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = "${var.public-subnet-2-cidr}"
   availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
 
   tags      = {
     Name    = "Public Subnet 2"
@@ -63,8 +60,7 @@ resource "aws_subnet" "public-subnet-2" {
 resource "aws_subnet" "public-subnet-3" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = "${var.public-subnet-3-cidr}"
-  availability_zone        = "us-east-1a"
-  map_public_ip_on_launch  = false
+  availability_zone        = "us-east-1c"
 
   tags      = {
     Name    = "Public Subnet 3"
@@ -99,11 +95,23 @@ resource "aws_route_table_association" "public-subnet-3-route-table-association"
   route_table_id      = aws_route_table.public-route-table.id
 }
 
+resource "aws_nat_gateway" "pubblic_subent-1" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.public-subnet-1.id
+}
+resource "aws_nat_gateway" "pubblic_subent-2" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.public-subnet-2.id
+}
+resource "aws_nat_gateway" "pubblic_subent-3" {
+  connectivity_type = "private"
+  subnet_id         = aws_subnet.public-subnet-3.id
+}
+
 resource "aws_subnet" "private-subnet-1" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = "${var.private-subnet-1-cidr}"
   availability_zone        = "us-east-1a"
-  map_public_ip_on_launch  = false
 
   tags      = {
     Name    = "Private Subnet 1"
@@ -114,7 +122,6 @@ resource "aws_subnet" "private-subnet-2" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = "${var.private-subnet-2-cidr}"
   availability_zone        = "us-east-1b"
-  map_public_ip_on_launch  = false
 
   tags      = {
     Name    = "Private Subnet 2"
@@ -124,10 +131,22 @@ resource "aws_subnet" "private-subnet-2" {
 resource "aws_subnet" "private-subnet-3" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = "${var.private-subnet-3-cidr}"
-  availability_zone        = "us-east-1a"
-  map_public_ip_on_launch  = false
+  availability_zone        = "us-east-1c"
 
   tags      = {
     Name    = "Private Subnet 3"
+  }
+}
+
+resource "aws_route_table" "private-route-table" {
+  vpc_id       = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet-gateway.id
+  }
+
+  tags       = {
+    Name     = "Private Route Table"
   }
 }
