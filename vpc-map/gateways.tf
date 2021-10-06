c82dd4127031477e1855a4a18ebfe8d7dd5e2d68
naveen <naveen@gmail.com>
2021-10-06 17:18:17 +0530

    vpc

--- vpc-map/gateways.tf
+++ vpc-map/gateways.tf
@@ -5,7 +5,7 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
-resource "aws_nat_gateway" "ngw" {
+resource "aws_nat_gateway" "nat" {
   connectivity_type = "private"
   subnet_id = aws_subnet.public[element(keys(aws_subnet.public), 0)].id
   tags = {
@@ -27,6 +27,17 @@ resource "aws_route_table" "private" {
   }
 }
 
+resource "aws_route" "public" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route" "private" {
+  route_table_id         = aws_route_table.private.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat.id
+}
 
 resource "aws_route_table_association" "public" {
   for_each  = aws_subnet.public
