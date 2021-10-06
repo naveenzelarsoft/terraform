3e393fccf3001757def5f0e882a6a0b4c532366a
naveen <naveen@gmail.com>
2021-10-06 18:48:55 +0530

    gcp

--- gcp/main.tf
+++ gcp/main.tf
@@ -1,3 +1,33 @@
+#terraform {
+#  required_providers {
+#    google = {
+#      source = "hashicorp/google"
+#      version = "3.5.0"
+#    }
+#  }
+#}
+#
+#provider "google" {
+#
+#  credentials = file("D:/Zelarsoft/naveenzelarsoft/TODO/terraform/key.json")
+#  project = "focus-mechanic-328207"
+#  region  = "us-central1"
+#}
+#resource "google_compute_instance" "vm_instance" {
+#  name         = "terraform-instance"
+#  machine_type = "f1-micro"
+#
+#  boot_disk {
+#    initialize_params {
+#      image = "debian-cloud/debian-9"
+#    }
+#  }
+#  network_interface {
+#    network = "default"
+#    access_config {
+#    }
+#  }
+#}
 terraform {
   required_providers {
     google = {
@@ -8,23 +38,13 @@ terraform {
 }
 
 provider "google" {
-
   credentials = file("D:/Zelarsoft/naveenzelarsoft/TODO/terraform/key.json")
+
   project = "focus-mechanic-328207"
   region  = "us-central1"
+  zone    = "us-central1-c"
 }
-resource "google_compute_instance" "vm_instance" {
-  name         = "terraform-instance"
-  machine_type = "f1-micro"
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-9"
-    }
-  }
-  network_interface {
-    network = "default"
-    access_config {
-    }
-  }
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
 }
