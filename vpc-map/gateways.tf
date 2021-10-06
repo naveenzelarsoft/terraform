349e6b50a91c045ccfcb9e1b8d1fb92acc4e08d7
naveen <naveen@gmail.com>
2021-10-06 17:33:37 +0530

    vpc

--- vpc-map/gateways.tf
+++ vpc-map/gateways.tf
@@ -9,44 +9,42 @@ resource "aws_nat_gateway" "nat" {
   connectivity_type = "private"
   subnet_id = aws_subnet.public[element(keys(aws_subnet.public), 0)].id
   tags = {
-    Name        = "Sample-nat"
+    Name        = "Sample-NAT"
   }
 }
 
-resource "aws_route_table" "public" {
+resource "aws_route_table" "public-route" {
   vpc_id = aws_vpc.vpc.id
+  route {
+    route_table_id = aws_route_table.public-route.id
+    cidr_block     = "0.0.0.0/0"
+    gateway_id     = aws_internet_gateway.igw.id
+  }
   tags = {
-    Name        = "public-route"
+    Name        = "Public-route"
   }
 }
 
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.vpc.id
-  tags = {
-    Name        = "private-route"
+resource "aws_route_table" "private-route" {
+  vpc_id       = aws_vpc.vpc.id
+  route {
+    route_table_id = aws_route_table.private-route.id
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.nat.id
   }
-}
-
-resource "aws_route" "public" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
-}
-
-resource "aws_route" "private" {
-  route_table_id         = aws_route_table.private.id
-  destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = aws_nat_gateway.nat.id
+  tags       = {
+    Name     = "Private-Route"
+  }
 }
 
 resource "aws_route_table_association" "public" {
   for_each  = aws_subnet.public
   subnet_id = aws_subnet.public[each.key].id
-  route_table_id = aws_route_table.public.id
+  route_table_id = aws_route_table.public-route.id
 }
 
 resource "aws_route_table_association" "private" {
   for_each  = aws_subnet.private
   subnet_id = aws_subnet.private[each.key].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private-route.id
 }
