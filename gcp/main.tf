08c66e1a645e649e7512d5ab39cf325ade9bb548
naveen <naveen@gmail.com>
2021-10-06 18:51:58 +0530

    gcp

--- gcp/main.tf
+++ gcp/main.tf
@@ -45,6 +45,6 @@ provider "google" {
   zone    = "us-central1-c"
 }
 
-resource "google_compute_network" "vpc_network" {
-  name = "terraform-network"
+resource "google_compute_network" "vm_instance" {
+  name = "default"
 }
