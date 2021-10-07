99e4527986d9e351af45fddce557745fb6a629ab
naveen <naveen@gmail.com>
2021-10-07 13:20:32 +0530

    gcp

--- vpc-map/main.tf
+++ vpc-map/main.tf
@@ -1,10 +1,10 @@
-terraform {
-  backend "s3" {
-    bucket = "terraform-bucket-for-tfstate-files"
-    key    = "vpc-map"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "terraform-bucket-for-tfstate-files"
+#    key    = "vpc-map"
+#    region = "us-east-1"
+#  }
+#}
 
 provider "aws" {
   region     = "us-east-1"
