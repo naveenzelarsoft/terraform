3694927b339afadb539f25e7840963e98c55de88
naveen <naveen@gmail.com>
2021-10-07 16:07:02 +0530

    gcp

--- ec2/instance.tf
+++ ec2/instance.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region = "us-east-1"
   shared_credentials_file = "C:/Users/naveen/.aws/credentials"
-  profile                 = "terraform"
+  profile                 = "windows"
 }
 resource "aws_instance" "sample" {
   ami          = "ami-0eb5f3f64b10d3e0e"
