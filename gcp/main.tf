10c7ad678889f8bd0804218d613e595ba3c76f97
naveen <naveen@gmail.com>
2021-10-07 14:48:15 +0530

    gcp

--- gcp/main.tf
+++ gcp/main.tf
@@ -9,7 +9,7 @@ terraform {
 
 provider "google" {
 
-  credentials = file("D:/Zelarsoft/naveenzelarsoft/TODO/terraform/key.json")
+  credentials = file("D:/Zelarsoft/naveenzelarsoft/TODO/key.json")
   project     = "focus-mechanic-328207"
   region      = "us-central1"
   zone        = "us-central1-c"
