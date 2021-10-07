863608ae8eacda1ddcfe76741be2219016f2a235
naveen <naveen@gmail.com>
2021-10-07 16:09:00 +0530

    gcp

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -1,5 +1,7 @@
 provider "aws" {
   region = "us-east-2"
+  shared_credentials_file = "C:/Users/naveen/.aws/credentials"
+  profile                 = "windows"
   version = "~> 3.61.0"
 }
 resource "aws_s3_bucket" "bommakanti" {
