2ec933ec998f5285cdc8b2667464063ac1971068
naveen <naveen@gmail.com>
2021-10-07 09:29:24 +0530

    gcp

--- vpc-map/variables.tf
+++ vpc-map/variables.tf
@@ -1,12 +1,10 @@
 variable "vpc_cidr" {
   type = string
-  description = "The IP range to use for the vpc"
   default = "10.0.0.0/16"
 }
 
 variable "public_subnet_numbers" {
   type = map(number)
-  description = "Map of AZ to a number that should be used for public subnets"
   default = {
     "us-east-1a" = 1
     "us-east-1b" = 2
@@ -16,7 +14,6 @@ variable "public_subnet_numbers" {
 
 variable "private_subnet_numbers" {
   type = map(number)
-  description = "Map of AZ to a number that should be used for private subnets"
   default = {
     "us-east-1a" = 4
     "us-east-1b" = 5
