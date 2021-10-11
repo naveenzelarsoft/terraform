e4ba0571886434078d74f0f069114022b7c4f82e
naveen <naveen@gmail.com>
2021-10-11 11:00:26 +0530

    file

--- s3/bucket.tf
+++ s3/bucket.tf
@@ -2,7 +2,6 @@ provider "aws" {
   region = "us-east-2"
   shared_credentials_file = "C:/Users/naveen/.aws/credentials"
   profile                 = "windows"
-  version = "~> 3.61.0"
 }
 resource "aws_s3_bucket" "bommakanti" {
   bucket = "bommakanti"
